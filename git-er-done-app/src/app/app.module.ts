786a250faca53317ba7addfbb69ce576f6212963
mmarkham128 <mmarkham128@gmail.com>
2020-08-31 22:30:12 -0500

    AAAAHHHHHHHHH

--- git-er-done-app/src/app/app.module.ts
+++ git-er-done-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+<<<<<<< Updated upstream
     AppComponent,
 <<<<<<< HEAD
 
@@ -15,6 +16,9 @@ import { AppComponent } from './app.component';
 =======
 >>>>>>> ac0303fbe00801a50ebb13ede201a8f4c40a364b
 >>>>>>> 44967f2583f6e25d49805fb41ebbf4f9fe06494c
+=======
+    AppComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
