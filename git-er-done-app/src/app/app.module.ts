ac0303fbe00801a50ebb13ede201a8f4c40a364b
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-08-31 23:02:15 -0400

    Update app.module.ts

--- git-er-done-app/src/app/app.module.ts
+++ git-er-done-app/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
   ],
   imports: [
     BrowserModule,
