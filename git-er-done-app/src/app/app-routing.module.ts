a2274082493555690a642b8638c51a4a6f47d28b
mmarkham128 <mmarkham128@gmail.com>
2020-09-08 16:57:31 -0500

    added bulma

--- git-er-done-app/src/app/app-routing.module.ts
+++ git-er-done-app/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+
+
 // imports for each of the route components
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { AdminFormComponent } from './admin/admin-form/admin-form.component';
+
+
 
 // create path and components for each of the pages
 const routes: Routes = [
