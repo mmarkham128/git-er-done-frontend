276f735edba1c48391798869581ff059e0a2433b
Konya Ferrell <race4Jesus@gmail.com>
2020-09-10 20:21:06 -0700

    Admin-form page
    updated the link for the page

--- git-er-done-app/src/app/app.module.ts
+++ git-er-done-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
-import { AdminFormComponent } from './admin-form/admin-form.component';
+import { AdminFormComponent } from './admin/admin-form/admin-form.component';
 
 @NgModule({
   declarations: [
