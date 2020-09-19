bc976961026ce3badc8a377d156b8d09fe2d9e66
Konya Ferrell <race4Jesus@gmail.com>
2020-09-19 13:23:23 -0700

    created component shells

---
+++ git-er-done-app/src/app/admin/restore-job/restore-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restore-job',
+  templateUrl: './restore-job.component.html',
+  styleUrls: ['./restore-job.component.css']
+})
+export class RestoreJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ git-er-done-app/src/app/admin/view-current-jobs/view-current-jobs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-current-jobs',
+  templateUrl: './view-current-jobs.component.html',
+  styleUrls: ['./view-current-jobs.component.css']
+})
+export class ViewCurrentJobsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
