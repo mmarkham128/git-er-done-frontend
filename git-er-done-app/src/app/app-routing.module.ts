5e93727ef5f13f8962a35a17f08d15985969bf80
Konya Ferrell <race4Jesus@gmail.com>
2020-09-19 19:15:46 -0700

    deleted some old components and added new components

--- git-er-done-app/src/app/app-routing.module.ts
+++ git-er-done-app/src/app/app-routing.module.ts
@@ -6,17 +6,36 @@ import { CommonModule } from '@angular/common';
 // imports for each of the route components
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { AdminFormComponent } from './admin/admin-form/admin-form.component';
+
+import { MyJobsComponent } from './employee/my-jobs/my-jobs.component';
+import { CompleteJobComponent } from './employee/complete-job/complete-job.component';
+import { CreateEmployeeComponent } from './signup/create-employee/create-employee.component';
+import { ViewEmployeeComponent } from './signup/view-employee/view-employee.component';
+import { EditEmployeeComponent } from './signup/edit-employee/edit-employee.component';
+import { CreateJobComponent } from './admin/create-job/create-job.component';
+import { EditJobComponent } from './admin/edit-job/edit-job.component';
+import { RestoreJobComponent } from './admin/restore-job/restore-job.component';
+import { ViewAllJobsComponent } from './admin/view-all-jobs/view-all-jobs.component';
+import { ViewCurrentJobsComponent } from './admin/view-current-jobs/view-current-jobs.component';
+import { ViewCompletedJobsComponent } from './admin/view-completed-jobs/view-completed-jobs.component';
 
 
 
 // create path and components for each of the pages
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent},
-  { path: 'signup', component: SignupPageComponent},
-  { path: 'admin-form', component: AdminFormComponent},
-  { path: 'login', component: LoginPageComponent }
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'my-jobs', component: MyJobsComponent },
+  { path: 'complete-job', component:CompleteJobComponent} ,
+  { path: 'signup', component: CreateEmployeeComponent },
+  { path: 'employee-profile', component: ViewEmployeeComponent },
+  { path: 'edit-employee', component: EditEmployeeComponent },
+  { path: 'create-job', component: CreateJobComponent },
+  { path: 'edit-job', component: EditJobComponent },
+  { path: 'restore-job', component: RestoreJobComponent },
+  { path: 'all-jobs', component: ViewAllJobsComponent } ,
+  { path: 'current-jobs', component: ViewCurrentJobsComponent },
+  { path: 'completed-jobs', component: ViewCompletedJobsComponent },
 ];
 
 @NgModule({
