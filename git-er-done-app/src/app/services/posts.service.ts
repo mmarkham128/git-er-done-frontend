78733170c12351be1a2e68ac292e45a6ebe6ed89
mmarkham128 <mmarkham128@gmail.com>
2020-09-23 17:17:52 -0500

    get routes
    added get routes for posts, rxjs, and angular forms

--- git-er-done-app/src/app/services/posts.service.ts
+++ git-er-done-app/src/app/services/posts.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Post } from '../models/posts'
+import { Injectable } from '@angular/core'
+import {Subject} from 'rxjs'
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 // check to make sure the url is correct - should be the path to the database
-  myPostsURL: string = "http://localhost:3000/posts"
+  myPostsURL: string = "http://localhost:3000/api/posts"
+  private posts: Post[]= [];
+  private postsUpdated = new Subject<Post[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    this.http.get<{message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+    .subscribe((postData) => {
+        this.posts = postData.posts
+        this.postsUpdated.next([...this.posts])
+    });
+    }
+
+    getPostUpdateListener() {
+      return this.postsUpdated.asObservable();
+  }
 }
