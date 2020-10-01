71cf5c62115c0728f5d0b007b76bec6e9451e2cf
Konya Ferrell <race4Jesus@gmail.com>
2020-10-01 14:52:56 -0700

    working on ngIf functionality - not working

--- git-er-done-app/src/app/admin/view-completed-jobs/view-completed-jobs.component.ts
+++ git-er-done-app/src/app/admin/view-completed-jobs/view-completed-jobs.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Subscription } from 'rxjs';
+import { Post } from '../../models/posts';
+import { PostsService } from '../../services/posts.service'
 
 @Component({
   selector: 'app-view-completed-jobs',
   templateUrl: './view-completed-jobs.component.html',
   styleUrls: ['./view-completed-jobs.component.css']
 })
+
 export class ViewCompletedJobsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  posts: Post []= []
+  private postsSub: Subscription;
+  
+    constructor(public postsService: PostsService) { }
+  
+    ngOnInit(){
+      this.postsService.getPosts()
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+          this.posts = posts;
+      });
   }
-
-}
+  ngOnDestroy(){
+      this.postsSub.unsubscribe();
+  }
+  }
