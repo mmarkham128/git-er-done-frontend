4d0cdf42d33f910b2c1602c3d33e43073161cbbb
LindseyDarnell29 <52871510+LindseyDarnell29@users.noreply.github.com>
2020-10-03 00:21:31 -0500

    view employee cards and TS coding

--- git-er-done-app/src/app/signup/view-employee/view-employee.component.ts
+++ git-er-done-app/src/app/signup/view-employee/view-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Subscription } from 'rxjs';
+import { User } from '../../models/users';
+import { UsersService } from '../../services/users.service'
 
 @Component({
   selector: 'app-view-employee',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEmployeeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  users: User []= []
+  private usersSub: Subscription;
+  
+    constructor(public usersService: UsersService) { }
+  
+    ngOnInit(){
+      this.usersService.getUsers()
+    this.usersSub = this.usersService.getUserUpdateListener()
+      .subscribe((users: User[]) => {
+          this.users = users;
+      });
   }
+  ngOnDestroy(){
+      this.usersSub.unsubscribe();
+  }
+  }
+  
 
-}
+
