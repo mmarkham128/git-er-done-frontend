b33a40da6d91504d99da7ba24ad25a46f0a47b23
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-10-12 02:19:24 -0400

    Authintaction with some goodies

--- git-er-done-app/src/app/app.component.ts
+++ git-er-done-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 
 @Component({
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'git-er-done-app';
+
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
